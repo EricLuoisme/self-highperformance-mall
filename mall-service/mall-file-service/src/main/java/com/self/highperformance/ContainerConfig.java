b22cd9531367a1b46ed15aa500207748e07ca365
lykismekk <lykismekk@outlook.com>
2021-11-10 14:21:03 +0800

    Ceph搭建成功

---
+++ mall-service/mall-file-service/src/main/java/com/self/highperformance/ContainerConfig.java
@@ -0,0 +1,39 @@
+package com.self.highperformance;
+
+import org.javaswift.joss.client.factory.AccountConfig;
+import org.javaswift.joss.client.factory.AccountFactory;
+import org.javaswift.joss.client.factory.AuthenticationMethod;
+import org.javaswift.joss.model.Account;
+import org.javaswift.joss.model.Container;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ContainerConfig {
+
+    private String username;
+    private String password;
+    private String authUrl;
+    private String defaultContainerName;
+
+    @Bean
+    public Account account() {
+        AccountConfig config = new AccountConfig();
+        config.setUsername(this.username);
+        config.setPassword(this.password);
+        config.setAuthUrl(this.authUrl);
+        config.setAuthenticationMethod(AuthenticationMethod.BASIC);
+        return new AccountFactory(config).createAccount();
+    }
+
+    @Bean
+    public Container container() {
+        Container newContainer = account().getContainer(defaultContainerName);
+        if (!newContainer.exists()) {
+            return newContainer.create();
+        }
+        return newContainer;
+    }
+
+
+}
