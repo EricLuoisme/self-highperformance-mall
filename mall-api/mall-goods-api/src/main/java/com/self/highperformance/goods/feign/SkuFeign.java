d61eec1a63bd8925e53c20580c63466125e4a93e
lykismekk <lykismekk@outlook.com>
2021-11-12 15:10:56 +0800

    使用feign接口, 允许Nginx通过Feign调用, 并且配置了Lua作为Nginx脚本, Nginx特定路径会走Redis缓存获取

---
+++ mall-api/mall-goods-api/src/main/java/com/self/highperformance/goods/feign/SkuFeign.java
@@ -0,0 +1,37 @@
+package com.self.highperformance.goods.feign;
+
+import com.self.highperformance.goods.model.Sku;
+import com.self.highperformance.resp.RespResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * Feign接口, 对应Sku服务的路径
+ */
+@FeignClient("mall-goods")
+public interface SkuFeign {
+
+    /**
+     * 根据推广分类查询推广产品列表, 因为是响应给Nginx缓存, 不使用Resp封装
+     */
+    @GetMapping("/sku/aditems/type")
+    List<Sku> typeItems(@RequestParam("id") Integer id);
+
+    /**
+     * 更新缓存
+     */
+    @PutMapping("/sku/aditems/type")
+    RespResult<List<Sku>> updTypeItems(@RequestParam("id") Integer id);
+
+    /**
+     * 清除缓存
+     */
+    @DeleteMapping("/sku/aditems/type")
+    RespResult delTypeItems(@RequestParam("id") Integer id);
+
+}
